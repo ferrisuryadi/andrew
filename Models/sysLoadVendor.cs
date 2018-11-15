41095cb94b94c683e9bfaddaf856bcf97cfac5c8
ferrisuryadi <ferri.suryadi@unicoin-mining.com>
2018-11-15 13:48:02 +0700

    cleaning

--- Models/sysLoadVendor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace fms.Models
-{
-    public class sysLoadVendor
-    {
-        public int sellingShipperId { get; set; }
-        public int sellingShipperDetailId { get; set; }
-    }
-}
